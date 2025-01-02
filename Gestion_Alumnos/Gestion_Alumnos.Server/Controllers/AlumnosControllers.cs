b2f6ea69899e30a80734c27488e80f58686b2bad
Leandro Bertero <leandro.bertero@hotmail.com>
2025-01-02 05:47:31 -0300

    EndPoints Get Post de Alumnos

--- Gestion_Alumnos/Gestion_Alumnos.Server/Controllers/AlumnosControllers.cs
+++ Gestion_Alumnos/Gestion_Alumnos.Server/Controllers/AlumnosControllers.cs
@@ -46,5 +46,49 @@ namespace Gestion_Alumnos.Server.Controllers
             }
         }
         #endregion
+
+        //Método Put
+        #region Put
+        //[HttpPut("{id:int}")]
+        //public async Task<ActionResult> Put(int id, [FromBody] Alumno entidad) 
+        //{
+        //    if (id != entidad.Id)
+        //    {
+        //        return BadRequest("Datos incorrectos");
+        //    }
+
+        //    var Dammy = await context.Alumnos.Where(e => e.Id == id).FirstOrDefaultAsync();
+
+        //    if (Dammy == null) 
+        //    {
+        //        return NotFound("No existe el alumno buscado.");    
+        //    }
+            
+        //    Dammy.Nombre = entidad.Nombre;
+        //    Dammy.Sexo = entidad.Sexo;
+        //    Dammy.FechaNacimiento = entidad.FechaNacimiento;
+        //    Dammy.Edad = entidad.Edad;
+        //    Dammy.CUIL = entidad.CUIL;
+        //    Dammy.Pais = entidad.Pais;
+        //    Dammy.Provincia = entidad.Provincia;
+        //    Dammy.TituloBase = entidad.TituloBase;
+        //    Dammy.CUS = entidad.CUS;
+        //    Dammy.Estado = entidad.Estado;
+
+        //    try
+        //    {
+        //        context.Alumnos.Update(Dammy);
+        //        await context.SaveChangesAsync();
+        //    }
+        //    catch (Exception e)
+        //    {
+
+        //        return BadRequest(e.Message);   
+        //        //throw;
+        //    }
+
+        //    return Ok();
+        //}
+        #endregion
     }
 }
