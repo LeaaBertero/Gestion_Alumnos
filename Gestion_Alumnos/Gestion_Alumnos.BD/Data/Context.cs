d89eca8bf4bc3d7da32a40a05f9fd6009b1819ad
Leandro Bertero <leandro.bertero@hotmail.com>
2025-01-02 03:12:33 -0300

    commit inicial

---
+++ Gestion_Alumnos/Gestion_Alumnos.BD/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Alumnos.BD.Data
+{
+    public class Context : DbContext
+    {
+
+    }
+}
