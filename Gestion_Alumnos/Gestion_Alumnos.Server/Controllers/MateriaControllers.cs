cdc2c187267af43e280efc348185b1622d13bfc8
Leandro Bertero <leandro.bertero@hotmail.com>
2025-01-02 06:00:48 -0300

    Controller Materia con EndPoint Get & Post

---
+++ Gestion_Alumnos/Gestion_Alumnos.Server/Controllers/MateriaControllers.cs
@@ -0,0 +1,52 @@
+﻿using Gestion_Alumnos.BD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Alumnos.BD.Data.Entidades;
+
+namespace Gestion_Alumnos.Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/Materia")]
+    public class MateriaControllers : ControllerBase
+    {
+        private readonly Context context;
+
+        //constructor
+        #region constructor
+        public MateriaControllers(Context context)
+        {
+            this.context = context;
+        }
+        #endregion
+
+
+        //Método Get
+        #region Get
+        [HttpGet]
+        public async Task<ActionResult<List<Materia>>> Get()
+        {
+            return await context.Materias.ToListAsync();
+        }
+        #endregion
+
+        //Método Post
+        #region Post
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Materia entidad)
+        {
+            try
+            {
+                context.Materias.Add(entidad);
+                await context.SaveChangesAsync();
+                return entidad.Id;
+            }
+            catch (Exception err)
+            {
+
+                return BadRequest(err.Message);
+            }
+        }
+        #endregion
+    }
+}
